07b128a168a807c6f445dc949d15f35692281a6a
Logan Fossenier <logan.fossenier@gmail.com>
2023-12-09 16:30:19 -0600

    feat: more random additions

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-x = [1, 2, 3, 4]
-print(x[:2])
+# list of movies (title, length, year)
+movies = [
+    ["The Holy Grail", 91, 1975],
+    ["The Life of Brian", 91, 1979],
+    ["The Meaning of Life", 107, 1983],
+]
+print([movies[i][0] for i in range(len(movies)) if movies[i][2] == 1975])
