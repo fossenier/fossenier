260f693282aa3a5d10ec783e6190ebd693199b83
Logan Fossenier <logan.fossenier@gmail.com>
2024-01-30 11:01:50 -0600

    feat: test for 145 a3q1

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
-x, y = 1, 2
+def copy3(data):
+    """
+    Copies the given list of data.
+    Preconditions:
+        :param data: a list
+        :param copy: a list with the same contents as data
+    Postconditions:
+        data becomes empty
+    :return: A copy of the list
+    """
+    copied = []
+    for i in range(len(data)):
+        d = data[i]
+        data.remove(d)
+        copied.append(d)
+    return copied
 
-*(x, y) += *(1, 2)
+
+x = [1, 2, 3]
+
+y = copy3(x)
+
 print(x, y)
