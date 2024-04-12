062c469af36e376fd044fc2138d4e0375745baef
Logan Fossenier <logan.fossenier@gmail.com>
2024-04-11 19:47:16 -0600

    feat: implement murderer selection

--- clue/clue.py
+++ clue/clue.py
@@ -1,33 +1,63 @@
 import os
 import sys
+
+from random import randint
 from typing import List
 
 BOARD_FILE = "board.csv"
 
 
 def main():
-    results = read_board()
-    print(results)
+    # set out the suspect, weapon, and room cards into stacks
+    suspects, weapons, rooms = read_board()
+
+    # TODO remove after testing
+    print("Suspects:", suspects)
+    print("Weapons:", weapons)
+    print("Rooms:", rooms)
+
+    # select a random card from each stack to be the murderer
+    murderer = pick_murderer(suspects, weapons, rooms)
+    print(murderer)
+
+
+def pick_murderer(
+    suspects: List[str], weapons: List[str], rooms: List[str]
+) -> List[str]:
+    """
+    Purpose:
+        Picks a random murderer (suspect, weapon, and room) from the provided card stacks.
+    Pre-conditions:
+        suspects List[str]: The suspect cards.
+        weapons List[str]: The weapon cards.
+        rooms List[str]: The room cards.
+    Post-conditions:
+        Modifies the initial lists by removing a card from each
+    Returns:
+        List[str]: A random suspect, weapon, and room card.
+    """
+    # pick the murderer, weapon, and location
+    murderer = []
+    for stack in (suspects, weapons, rooms):
+        murderer.append(stack.pop(randint(0, len(stack) - 1)))
+    return murderer
 
 
 def read_board(provided_path: str = BOARD_FILE) -> List[List[str]]:
     """
     Purpose:
-         Reads a clue game configuration file to extract the names of suspects, weapons, and rooms.
-
+         Reads a clue game configuration file to extract the card stacks of suspects, weapons, and rooms.
      Pre-conditions:
         provided_path str:
         The path to the file containing the clue game configuration. If None, a default path is used.
         The file should contain 6 lines, with the first 3 lines being comments and the last 3 lines
         containing comma-separated values representing suspects, weapons, and rooms.
-
      Post-conditions:
          The function will terminate the program if the specified file does not exist.
-
      Returns:
-         List[str]: The suspects.
-         List[str]: The weapons.
-         List[str]: The rooms.
+         List[str]: The suspect cards.
+         List[str]: The weapon cards.
+         List[str]: The room cards.
     """
     path = provided_path if provided_path else BOARD_FILE
 
