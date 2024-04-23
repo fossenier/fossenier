f9a5f376a5551f402b0c01c93b004e6c5a04441d
Logan Fossenier <logan.fossenier@gmail.com>
2024-04-22 21:13:01 -0600

    feat: read clue board and print to file

--- clue/clue_manual.py
+++ clue/clue_manual.py
@@ -23,9 +23,19 @@ SUSPECTS = [
 ]
 WEAPONS = ["Candlestick", "Knife", "Lead Pipe", "Revolver", "Rope", "Wrench"]
 
+IMAGE = [
+    [False, True, False, False, True, False],
+    [False, False, True, False, False, True],
+    [False, True, False, True, False, False],
+    [False, False, True, False, True, False],
+    [True, False, False, True, False, True],
+    [False, True, False, False, True, False],
+]
+
 
 def main():
-    output_image("clue_board_1.png")
+    board = read_board("clue_board_1.csv")
+    draw_board("clue_board_1.png", board)
     # proto_print_board()
 
 
@@ -35,57 +45,46 @@ def proto_print_board():
             print(line)
 
 
-def output_image(filename):
+def read_board(filename):
+    with open(filename, "r") as f:
+        board = []
+        for line in f:
+            row = line.strip().split(",")
+            board.append(row)
+    return board
+
+
+def draw_board(filename, board):
     from PIL import Image, ImageDraw
 
     cell_size = 50
     cell_border = 2
 
+    width, height = len(board[0]), len(board)
+
     # Create a blank canvas
-    img = Image.new("RGBA", (50 * cell_size, 50 * cell_size), "black")
+    img = Image.new("RGBA", (width * cell_size, height * cell_size), "black")
     draw = ImageDraw.Draw(img)
 
-    # solution = self.solution[1] if self.solution is not None else None
-    # for i, row in enumerate(self.walls):
-    #     for j, col in enumerate(row):
+    for i, row in enumerate(board):
+        for j, tile in enumerate(row):
+            if tile == " ":
+                fill = (226, 200, 60)
+            elif tile == "x":
+                fill = (40, 39, 41)
 
-    #         # Walls
-    #         if col:
-    #             fill = (40, 40, 40)
-
-    #         # Start
-    #         elif (i, j) == self.start:
-    #             fill = (255, 0, 0)
-
-    #         # Goal
-    #         elif (i, j) == self.goal:
-    #             fill = (0, 171, 28)
-
-    #         # Solution
-    #         elif solution is not None and show_solution and (i, j) in solution:
-    #             fill = (220, 235, 113)
-
-    #         # Explored
-    #         elif solution is not None and show_explored and (i, j) in self.explored:
-    #             fill = (212, 97, 85)
-
-    #         # Empty cell
-    #         else:
-    #             fill = (237, 240, 252)
-
-    #         # Draw cell
-    #         draw.rectangle(
-    #             (
-    #                 [
-    #                     (j * cell_size + cell_border, i * cell_size + cell_border),
-    #                     (
-    #                         (j + 1) * cell_size - cell_border,
-    #                         (i + 1) * cell_size - cell_border,
-    #                     ),
-    #                 ]
-    #             ),
-    #             fill=fill,
-    #         )
+            draw.rectangle(
+                (
+                    [
+                        (j * cell_size + cell_border, i * cell_size + cell_border),
+                        (
+                            (j + 1) * cell_size - cell_border,
+                            (i + 1) * cell_size - cell_border,
+                        ),
+                    ]
+                ),
+                fill=fill,
+            )
 
     img.save(filename)
 
