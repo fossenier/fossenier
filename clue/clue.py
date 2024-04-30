9d56f50ea066fb87818e824108854dcd9872d7fe
Logan Fossenier <logan.fossenier@gmail.com>
2024-04-29 21:24:10 -0600

    feat: create a Clue class to store board details

--- clue/clue.py
+++ clue/clue.py
@@ -3,6 +3,39 @@ This is a second slice of Clue that is focused on simply giving instructions.
 """
 
 
+class Clue:
+    def __init__(self):
+        self.ROOMS = [
+            "Ballroom",
+            "Billiard Room",
+            "Conservatory",
+            "Dining Room",
+            "Hall",
+            "Kitchen",
+            "Library",
+            "Lounge",
+            "Study",
+        ]
+        self.SUSPECTS = [
+            "Colonel Mustard",
+            "Miss Scarlet",
+            "Mr. Green",
+            "Mrs. Peacock",
+            "Mrs, White",
+            "Professor Plum",
+        ]
+        self.WEAPONS = [
+            "Candlestick",
+            "Knife",
+            "Lead Pipe",
+            "Revolver",
+            "Rope",
+            "Wrench",
+        ]
+        self.suspect_order = []
+        self.cpu_suspect = None
+
+
 def main():
     pass
 
