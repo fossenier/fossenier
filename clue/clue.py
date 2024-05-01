43562511078f3ac51667606c6447c022aae890b6
Logan Fossenier <logan.fossenier@gmail.com>
2024-04-30 21:16:05 -0600

    chore: refactor the code for logic and readability

--- clue/clue.py
+++ clue/clue.py
@@ -5,32 +5,108 @@ This is a second slice of Clue that is focused on simply giving instructions.
 
 class Clue:
     def __init__(self, game_path):
-        self.board = []
-        self.rooms = []
-        self.suspects = []
-        self.weapons = []
-        self.suspect_order = []
-        self.cpu_location = (None, None)
-        self.cpu_suspect = None
-        self.last_handled_suspect = None
+        self.board = []  # the game board
+        self.cpu_location = (None, None)  # the CPU location
+        self.cpu_suspect = None  # the CPU suspect
+        self.rooms = []  # list of room names
+        self.suspects = {}  # key: suspect name, value: (x, y) starting position
+        self.suspect_order = []  # the order of the players
+        self.weapons = []  # list of weapon names
 
         self.__read_board_data(game_path)
-        self.__read_board(game_path)
 
-    def __read_board(self, game_path):
-        with open(game_path, "r") as f:
-            for line in f:
-                # read row
-                row = line.rstrip().split(",")
-                # remove suspects from the board
-                for i, tile in enumerate(row):
-                    if tile in self.suspects:
-                        row[i] = " "
-                self.board.append(row)
+    def __get_tile_position(self, tile):
+        """
+        Purpose:
+            Get the position of a tile on the board.
+        Pre-conditions:
+            str tile: the tile to find.
+        Post-conditions:
+            tuple: the position of the tile.
+        Returns:
+            tuple: the position of the tile.
+        """
+        for y, row in enumerate(self.board):
+            for x, board_tile in enumerate(row):
+                if board_tile == tile:
+                    return (x, y)
+
+    def __prompt_cpu_suspect(self):
+        """
+        Purpose:
+            Prompts the user to select the CPU suspect.
+        Pre-conditions:
+            self.suspect_order must have been populated.
+        Post-conditions:
+            self.cpu_suspect: the CPU suspect.
+        Returns:
+            None.
+        """
+        # get the CPU suspect from the user
+        cpu_suspect = None
+        while cpu_suspect not in self.suspect_order:
+            cpu_suspect = input(f"Enter the CPU suspect({self.suspect_order}): ")
+
+        # save to the object the suspect as named in self.suspect_order
+        for suspect in self.suspect_order:
+            if cpu_suspect.lower() in suspect.lower():
+                self.cpu_suspect = suspect
+
+    def __prompt_game_order(self):
+        """
+        Purpose:
+            Prompts the user to enter the player order. The players entered must be
+            contained as characters within the suspects list.
+        Pre-conditions:
+            self.suspects must have been populated.
+        Post-conditions:
+            self.suspect_order: a list of the suspects in the order they are playing.
+        Returns:
+            None.
+        """
+        is_valid_order = False
+        # get the order of the players
+        while not is_valid_order:
+            print("Enter the order of the players, separated by commas.")
+            user_order = [suspect.strip() for suspect in input().split(",")]
+            validated_suspects = []
+
+            actual_suspects = [key for key in self.suspects.keys()]
+            for user_suspect in user_order:
+                # check if the suspect is valid
+                for actual_suspect in actual_suspects:
+                    # accept the suspect if it is a substring of the actual suspect (Green -> Mr. Green)
+                    if user_suspect.lower() in actual_suspect.lower():
+                        validated_suspects.append(actual_suspect)
+                        actual_suspects.remove(actual_suspect)
+                        break
+
+            # make sure 2 or more players are in the game
+            if len(validated_suspects) < 2:
+                print(
+                    f"Error: you must have at least 2 valid players. You entered {validated_suspects}."
+                )
+                continue
+
+            # use the validated order
+            self.suspect_order = validated_suspects
+            is_valid_order = True
 
     def __read_board_data(self, game_path):
+        """
+        Purpose:
+            Read the clue game board from a specified path.
+        Pre-conditions:
+            str game_path: the path to the game board. MUST BE properly formatted.
+        Post-conditions:
+            self.board: a list of lists representing the game board (players are removed).
+            self.rooms: all of the rooms in the game.
+            self.suspects: all of the suspects in the game.
+            self.weapons: all of the weapons in the game.
+        Returns:
+            None.
+        """
         with open(game_path, "r") as f:
-
             # first line is weapons
             self.weapons = f.readline().rstrip().split(",")
 
@@ -48,63 +124,39 @@ class Clue:
                             or x_coord == 0
                             or x_coord == len(tiles) - 1
                         ):
-                            self.suspects.append(tile)
+                            self.suspects[tile] = (x_coord, y_coord)
                         # otherwise it's a room
                         else:
                             if tile not in self.rooms:
                                 self.rooms.append(tile)
 
-    def __get_game_order(self):
-        valid_order = False
-        # get the order of the players
-        while not valid_order:
-            print("Enter the order of the players, separated by commas.")
-            order = [suspect.strip() for suspect in input().split(",")]
-            valid_suspects = []
-
-            checklist = self.suspects.copy()
-            for suspect in order:
-                # check if the suspect is valid
-                for check in checklist:
-                    # it is valid
-                    if suspect.lower() in check.lower():
-                        valid_suspects.append(check)
-                        checklist.remove(check)
-                        break
-
-            # make sure 2 or more players are in the game
-            if len(self.suspects) - len(checklist) < 2:
-                print(f"Error: you must have at least 2 players.")
-                continue
-
-            # the suspects left in the checklist are unused
-            self.suspect_order = valid_suspects
-            self.last_handled_suspect = self.suspect_order[-1]  # last player
-            valid_order = True
-
-    def __get_cpu_location(self):
-        self.cpu_location
-
-    def __get_cpu_suspect(self):
-        cpu_suspect = None
-        while cpu_suspect not in self.suspect_order:
-            cpu_suspect = input(f"Enter the CPU suspect({self.suspect_order}): ")
-        self.cpu_suspect = cpu_suspect
-
-    def __get_active_suspect(self):
-        last_index = self.suspect_order.index(self.last_handled_suspect)
-        current_index = (last_index + 1) % len(self.suspect_order)
-        return self.suspect_order[current_index]
+            # save the board to the object
+            for line in f:
+                # read row
+                row = line.rstrip().split(",")
+                # remove suspects from the board
+                for i, tile in enumerate(row):
+                    if tile in self.suspects.keys():
+                        row[i] = " "
+                self.board.append(row)
 
     def initialize_game(self):
+        """
+        Purpose:
+            Initializes the game with the two required user inputs before the main loop.
+        Pre-conditions:
+            None.
+        Post-conditions:
+            self.suspect_order, self.cpu_suspect are populated.
+        Returns:
+            None.
+        """
         print("Welcome to Clue!")
-        self.__get_game_order()
-        self.__get_cpu_suspect()
-        self.__get_cpu_location()
+        self.__prompt_game_order()
+        self.__prompt_cpu_suspect()
 
     def run_game(self):
-        active_suspect = self.__get_active_suspect()
-        if 
+        pass
 
 
 def main():
