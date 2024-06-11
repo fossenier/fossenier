9ef27d5203f020ef7b7629bc14d4d1e1366e52ca
Logan Fossenier <logan.fossenier@gmail.com>
2024-06-11 14:50:23 -0600

    feat: implement transaction parsing in a way that loops the document once

--- banking_pdf/banking.py
+++ banking_pdf/banking.py
@@ -80,7 +80,7 @@ class ScotiabankPDF(object):
         self.populate_location_data()
         
         for _, transaction in self.transaction_rows.items():
-            # print(transaction.date, transaction.original_statement, transaction.amount)
+            print(transaction.date, transaction.original_statement, transaction.amount)
             pass
 
         # transaction data
@@ -100,21 +100,25 @@ class ScotiabankPDF(object):
             """
             Checks the y0 and y1 in self.transaction_rows for a transaction.
             """
-            ROW_Y0 = 0.5
-            ROW_Y1 = 50
+            ROW_Y0 = 10
+            ROW_Y1 = 100
             for (row_y0, row_y1), transaction in self.transaction_rows.items():
-                if row_y0 - ROW_Y0 < y0 and row_y1 + ROW_Y1 > y1:
+                if abs(row_y0 - y0) < ROW_Y0 and abs(row_y1 - y1) < ROW_Y1:
                     return transaction
 
         DATE_X0 = 1
         DATE_X1 = 20
         TRANSACTION_X0 = 1
         TRANSACTION_X1 = 180
+        AMOUNT_X0 = 50 
+        AMOUNT_X1 = 5
         for page_layout in self.pages:
             for element in page_layout:
                 # save coordinates for the desired columns
                 if isinstance(element, LTTextBoxHorizontal):
                     text = element.get_text().strip()
+                    # if len(text) >= 6:
+                    #     print(text, "\n")
 
                     # grab the year from near the top of the document
                     if not self.year and text.startswith("Opening Balance on "):
@@ -135,10 +139,9 @@ class ScotiabankPDF(object):
                     # NOTE transactions appear below the date column header
                     elif (
                         self.date_column
-                        and self.date_column[0] - DATE_X0 < element.x0
-                        and self.date_column[1] + DATE_X1 > element.x1
+                        and abs(self.date_column[0] - element.x0) < DATE_X0
+                        and abs(self.date_column[1] - element.x1) < DATE_X1
                     ):
-                        print(text)
                         date = datetime.strptime(f"{text} {self.year}", "%b %d %Y")
                         self.transaction_rows[(element.y0, element.y1)] = Transaction(
                             date=date
@@ -147,10 +150,9 @@ class ScotiabankPDF(object):
                     # populate the transaction original statement
                     elif (
                         self.transaction_column
-                        and self.transaction_column[0] - TRANSACTION_X0 < element.x0
-                        and self.transaction_column[1] + TRANSACTION_X1 > element.x1
+                        and abs(self.transaction_column[0] - element.x0) < TRANSACTION_X0
+                        and abs(self.transaction_column[1] - element.x1) < TRANSACTION_X1
                     ):
-                        # print(text)
                         transaction = get_transaction(element.y0, element.y1)
                         if transaction:
                             transaction.original_statement = text
@@ -158,16 +160,16 @@ class ScotiabankPDF(object):
                     # populate the transaction amount
                     elif (
                         self.deposit_column
-                        and self.deposit_column[0] - TRANSACTION_X0 < element.x0
-                        and self.deposit_column[1] + TRANSACTION_X1 > element.x1
+                        and abs(self.deposit_column[0] - element.x0) < AMOUNT_X0
+                        and abs(self.deposit_column[1] - element.x1) < AMOUNT_X1
                     ):
                         transaction = get_transaction(element.y0, element.y1)
                         if transaction:
                             transaction.amount = text.replace(",", "")
                     elif (
                         self.withdrawal_column
-                        and self.withdrawal_column[0] - TRANSACTION_X0 < element.x0
-                        and self.withdrawal_column[1] + TRANSACTION_X1 > element.x1
+                        and abs(self.withdrawal_column[0] - element.x0) < AMOUNT_X0
+                        and abs(self.withdrawal_column[1] - element.x1) < AMOUNT_X1
                     ):
                         transaction = get_transaction(element.y0, element.y1)
                         if transaction:
