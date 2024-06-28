da5749177bf23da9d495b66f99d4eda4672c73b5
Logan Fossenier <logan.fossenier@gmail.com>
2024-06-27 22:51:19 -0600

    feat: simulate Linkedin post poll for curiosity sake

--- test.py
+++ test.py
@@ -1,2 +1,24 @@
-x = [1, 2, 3, 4, 3, 5]
-print(list(set(x)))
+# linkedin question
+# given an urn (2 red 1 blue balls)
+# draw balls, if red, put back a blue, if blue put back a blue
+# how many draws until they are all blue?
+
+from random import randint
+
+SIMULATIONS = 1000000
+
+
+def main():
+    draws = 0
+    for _ in range(SIMULATIONS):
+        # True is blue, red is False
+        bag = [False, False, True]
+        while False in bag:
+            draws += 1
+            bag.pop(randint(0, 2))
+            bag.append(True)
+    print(f"Average draws: {draws / SIMULATIONS}")
+
+
+if __name__ == "__main__":
+    main()
