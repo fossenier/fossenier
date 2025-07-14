9685e515fdcdf7ecec908d1eef0f0d000924f5f7
Logan Fossenier <logan.fossenier@gmail.com>
2025-07-14 14:10:21 -0600

    feat: complete analysis

--- horserace.py
+++ horserace.py
@@ -3,7 +3,8 @@ import random
 
 
 COIN_VALUE = 5
-PLAYER_COUNT = 7
+PLAYER_COUNT = 6
+SIM_COUNT = 8000
 
 # deal cards
 # kill horses
@@ -67,7 +68,7 @@ class Deck:
         # 2, 3, 4, ..., 10, J, Q
         for i in range(2, 13):
             # four suits of each card
-            for i in range(4):
+            for _ in range(4):
                 self.cards.append(i)
 
         self.shuffle()
@@ -147,119 +148,250 @@ class Player:
 
 
 def main():
+
+    stats = {}
+    stats["avg_pot"] = 0
+    stats["avg_pot_feed"] = 0
+    stats["avg_pot_30_feed"] = 0
+    for i in range(PLAYER_COUNT):
+        stats[f"elimination_{i}_avg"] = 0
+    for i in range(2, 13):
+        stats[f"%_{i}_win"] = 0
+    for i in range(2, 13):
+        stats[f"%_{i}_kill"] = 0
+
     board = Board()
     deck = Deck()
-    players = [Player([]) for _ in range(PLAYER_COUNT)]
-    dealer = 0
-    assert PLAYER_COUNT >= 2
 
-    round_pot_carryover = 0
+    for sim in range(SIM_COUNT):
+        players = [Player([]) for _ in range(PLAYER_COUNT)]
+        dealer = 0
+        assert PLAYER_COUNT >= 2
 
-    # TODO make this a while loop until eliminations
-    game_over = False
-    while not game_over:
-        # Current players
-        active_players = len(players)
+        round_pot_carryover = 0
 
-        round_pot = round_pot_carryover
-        # Deal out hands for each player
-        hands = deck.deal(active_players)
-        for i in range(active_players):
-            players[i].redeal(hands[i])
+        stat_entry = {}
+        stat_entry["eliminations"] = {}
+        player_amounts = range(1, PLAYER_COUNT)
+        stat_entry["horse_win"] = {}
+        stat_entry["horse_kill"] = {}
+        for i in range(2, 13):
+            stat_entry["horse_win"][i] = 0
+        for i in range(2, 13):
+            stat_entry["horse_kill"][i] = 0
+        stat_entry["horse_cost"] = {}
+        for i in range(2, 13):
+            stat_entry["horse_cost"][i] = 0
+        stat_entry["pot amount"] = 0
+        stat_entry["avg_cost_person"] = 0
+        stat_entry["avg_cost_person_30"] = 0
 
-        # First player
-        active_player = (dealer + 1) % active_players
+        game_over = False
+        round_count = 0
+        while not game_over:
+            # Current players
+            active_players = len(players)
+            if active_players == 1:
+                game_over = True
+                break
 
-        # Kill horses
-        horse_kills = 0
-        while horse_kills < 4:
-            # Perform 2d6 roll
-            roll = random.randint(1, 6) + random.randint(1, 6)
-            # The horse is alive, kill it from all hands
-            if not board.horses[roll].killed:
-                # Kill values are 5, 10, 15, 20
-                kill_value = COIN_VALUE * (horse_kills + 1)
-                # Set the horse to be killed
-                board.horses[roll].kill(kill_value)
-                horse_kills += 1
-                # Kill this horse for each player
-                for player in players:
-                    # See what they chip in to the pot
-                    pot_addition = player.kill(roll, kill_value)
+            round_pot = round_pot_carryover
+            # Deal out hands for each player
+            hands = deck.deal(active_players)
+            for i in range(active_players):
+                players[i].redeal(hands[i])
+
+            # First player
+            active_player = (dealer + 1) % active_players
+
+            # Kill horses
+            horse_kills = 0
+            while horse_kills < 4:
+                # Perform 2d6 roll
+                roll = random.randint(1, 6) + random.randint(1, 6)
+                # The horse is alive, kill it from all hands
+                if not board.horses[roll].killed:
+                    stat_entry["horse_kill"][roll] += 1
+                    # Kill values are 5, 10, 15, 20
+                    kill_value = COIN_VALUE * (horse_kills + 1)
+                    # Set the horse to be killed
+                    board.horses[roll].kill(kill_value)
+                    horse_kills += 1
+                    # Kill this horse for each player
+                    for player in players[:]:
+                        # See what they chip in to the pot
+                        pot_addition = player.kill(roll, kill_value)
+                        round_pot += pot_addition
+                        stat_entry["horse_cost"][roll] += pot_addition
+                        # Track eliminations
+                        if player.eliminated:
+                            if players.index(player) == active_player:
+                                active_player -= 1
+                            players.remove(player)
+                            active_players -= 1
+                            if active_players == 1:
+                                game_over = True
+                                break
+                    if game_over:
+                        break
+                # The horse is dead, active player pays for the roll
+                else:
+                    pot_addition = players[active_player].pay(board.horses[roll].value)
                     round_pot += pot_addition
+                    stat_entry["horse_cost"][roll] += pot_addition
                     # Track eliminations
-                    if player.eliminated:
-                        players.remove(player)
+                    if players[active_player].eliminated:
+                        players.pop(active_player)
                         active_players -= 1
+                        active_player -= 1
                         if active_players == 1:
                             game_over = True
                             break
                 if game_over:
                     break
-            # The horse is dead, active player pays for the roll
-            else:
-                pot_addition = players[active_player].pay(board.horses[roll].value)
-                round_pot += pot_addition
-                # Track eliminations
-                if players[active_player].eliminated:
-                    players.pop(active_player)
-                    active_players -= 1
-                    if active_players == 1:
-                        game_over = True
-                        break
+                # Update the current player
+                active_player = (active_player + 1) % active_players
             if game_over:
                 break
-            # Update the current player
-            active_player = (active_player + 1) % active_players
-        if game_over:
-            break
 
-        round_over = False
-        winning_horse = None
-        while not round_over:
-            roll = random.randint(1, 6) + random.randint(1, 6)
-            if board.horses[roll].killed:
-                pot_addition = players[active_player].pay(board.horses[roll].value)
-                round_pot += pot_addition
-                # Track eliminations
-                if players[active_player].eliminated:
-                    players.pop(active_player)
-                    active_players -= 1
-                    if active_players == 0:
-                        game_over = True
-                        break
-            else:
-                round_over = board.horses[roll].advance()
-                winning_horse = roll
+            round_over = False
+            winning_horse = None
+            while not round_over:
+                roll = random.randint(1, 6) + random.randint(1, 6)
+                if board.horses[roll].killed:
+                    pot_addition = players[active_player].pay(board.horses[roll].value)
+                    round_pot += pot_addition
+                    stat_entry["horse_cost"][roll] += pot_addition
+                    # Track eliminations
+                    if players[active_player].eliminated:
+                        players.pop(active_player)
+                        active_players -= 1
+                        active_player -= 1
+                        if active_players == 0:
+                            game_over = True
+                            break
+                else:
+                    round_over = board.horses[roll].advance()
+                    winning_horse = roll
+                if game_over:
+                    break
+                # Advance to the next player
+                active_player = (active_player + 1) % active_players
             if game_over:
                 break
-            # Advance to the next player
-            active_player = (active_player + 1) % active_players
-        if game_over:
-            break
 
-        # Pay out the pot
-        coins = round_pot // COIN_VALUE
-        # Count how many of the winning horse is in hands (because sometimes
-        # a person will be eliminated before collecting on a winning horse)
-        winning_horses = sum(player.horse_count(winning_horse) for player in players)
-        if winning_horses == 0:
-            round_pot_carryover = coins * COIN_VALUE
-        else:
-            # Payouts must be equal, based on how many winning horses are in hands (usually 4)
-            round_pot_carryover = (coins % winning_horses) * COIN_VALUE
-            payout_value = (coins // winning_horses) * COIN_VALUE
+            # Pay out the pot
+            stat_entry["pot amount"] += round_pot
+            stat_entry["avg_cost_person"] += round_pot / active_players
+            if round_count < 30:
+                stat_entry["avg_cost_person_30"] += round_pot / active_players
+            coins = round_pot // COIN_VALUE
+            # Count how many of the winning horse is in hands (because sometimes
+            # a person will be eliminated before collecting on a winning horse)
+            winning_horses = sum(
+                player.horse_count(winning_horse) for player in players
+            )
+            if winning_horses == 0:
+                round_pot_carryover = coins * COIN_VALUE
+            else:
+                # Payouts must be equal, based on how many winning horses are in hands (usually 4)
+                round_pot_carryover = (coins % winning_horses) * COIN_VALUE
+                payout_value = (coins // winning_horses) * COIN_VALUE
 
-            # Payout players, those who have the horse
-            for player in players:
-                player.payout(winning_horse, payout_value)
+                # Payout players, those who have the horse
+                for player in players:
+                    player.payout(winning_horse, payout_value)
 
-        dealer = (dealer + 1) % active_players
-        board.reset()
-        deck.shuffle()
+            dealer = (dealer + 1) % active_players
+            board.reset()
+            deck.shuffle()
+            round_count += 1
+            active_players = len(players)
 
-    print(players[0].wallet.coins)
-    print(len(players))
+            for player_amount in player_amounts:
+                if active_players == player_amount:
+                    try:
+                        stat_entry["eliminations"][PLAYER_COUNT - player_amount]
+                        # Nice, it exists, don't update it
+                    except:
+                        stat_entry["eliminations"][
+                            PLAYER_COUNT - player_amount
+                        ] = round_count
+                    for i in range(1, PLAYER_COUNT):
+                        try:
+                            if PLAYER_COUNT - player_amount - i <= 0:
+                                break
+                            stat_entry["eliminations"][PLAYER_COUNT - player_amount - i]
+                            # last player amount was added, good
+                            break
+                        except:
+                            stat_entry["eliminations"][
+                                PLAYER_COUNT - player_amount - i
+                            ] = round_count
+            stat_entry["horse_win"][winning_horse] += 1
+
+        if sim % 500 == 0:
+            print(f"Sim: {sim}")
+
+        stat_entry["eliminations"][PLAYER_COUNT - 1] = round_count
+        for i in range(1, PLAYER_COUNT):
+            try:
+                if PLAYER_COUNT - 1 - i <= 0:
+                    break
+                stat_entry["eliminations"][PLAYER_COUNT - 1 - i]
+                # last player amount was added, good
+                break
+            except:
+                stat_entry["eliminations"][PLAYER_COUNT - 1 - i] = round_count
+        #######
+        avg_pot = stat_entry["pot amount"] / round_count
+        # print(f"Average amount of the pot each round: {avg_pot}")
+        stats["avg_pot"] += avg_pot
+
+        avg_pot_feed = stat_entry["avg_cost_person"] / round_count
+        # print(f"Average amount the player feeds the pot: {avg_pot_feed}")
+        stats["avg_pot_feed"] += avg_pot_feed
+
+        avg_pot_30_feed = stat_entry["avg_cost_person_30"] / 30
+        # print(
+        #     f"Average amount the player feeds the pot in the first 30 rounds: {avg_pot_30_feed}"
+        # )
+        stats["avg_pot_30_feed"] += avg_pot_30_feed
+
+        for i in range(1, PLAYER_COUNT):
+            stats[f"elimination_{i}_avg"] += stat_entry["eliminations"][i]
+
+        for i in range(2, 13):
+            percent = stat_entry["horse_win"][i] / round_count * 100
+            # print(f"% chance that horse {i} wins: {percent:.2f}")
+            stats[f"%_{i}_win"] += percent
+
+        for i in range(2, 13):
+            percent = stat_entry["horse_kill"][i] / round_count * 100
+            # print(f"% chance that horse {i} is killed: {percent:.2f}")
+            stats[f"%_{i}_kill"] += percent
+        #######
+    for k in ["avg_pot", "avg_pot_feed", "avg_pot_30_feed"]:
+        print(f"{k}: {stats[k] / SIM_COUNT}")
+
+    for i in range(1, PLAYER_COUNT):
+        print(f"elimination_{i}_avg: {stats[f"elimination_{i}_avg"] / SIM_COUNT}")
+    for i in range(2, 13):
+        print(f"%_{i}_win: {stats[f"%_{i}_win"] / SIM_COUNT}")
+    for i in range(2, 13):
+        print(f"%_{i}_kill: {stats[f"%_{i}_kill"] / SIM_COUNT}")
+
+    # total_avg_cost = 0
+    # for i in range(2, 13):
+    #     stat_entry["horse_cost"][i] /= round_count
+    #     stat_entry["horse_cost"][i] /= PLAYER_COUNT
+    #     total_avg_cost += stat_entry["horse_cost"][i]
+    # print(f"tot avg {total_avg_cost}")
+
+    # print(players[0].wallet.coins)
+    # print(len(players))
+    # print(round_count)
+    # print(stat_entry)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print(17 // 4)
+x = [13.1, 25, 35.5, 44.6, 52.5]
+y = 59.3
+total = sum(x) + sum(x) + y
+
+for i in x:
+    print(i / total * 100)
+print(y / total * 100)
