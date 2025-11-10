96c519f3648e47fc9fda782ecddfbdd4573f679f
Logan Fossenier <logan.fossenier@gmail.com>
2025-11-10 10:53:48 -0600

    feat: implement waypoint json parsing
    The route data can now be parsed and each waypoint is read in
    along the journey.

--- asteroids/asteroids.py
+++ asteroids/asteroids.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from typing import List
+from typing import Dict, List, Set, Tuple
 
 # Speed calculator of platform
 # https://www.desmos.com/calculator/eykhbatbn6
@@ -19,22 +19,68 @@ asteroid_data = {
     "medium-carbonic-asteroid"
 }
 
-class Asteroid:
-    name: str
+class Planet:
+    rates: List[Tuple[str, float]] # asteroid per second at planet
 
 class Waypoint:
-    distance: int
-    asteroids: List[Asteroid]
+    distance: float # % of the way from source to dest for route
+    rates: Set[Tuple[str, float]] # asteroid per second at distance
+
+    def __init__(self, distance: float):
+        self.distance = distance
+        self.rates = set()
+
+    def add_rate(self, asteroid: str, rate: float):
+        self.rates.add((asteroid, rate))
+
+    def __str__(self) -> str:
+        output = ""
+        output += "Waypoint\n"
+        output += f"distance: {self.distance}\n"
+        output += f"asteroid spawn rates (asteroids per second):\n"
+        # alphabetical by asteroid name
+        for rate in sorted(self.rates, key=lambda entry: entry[0]):
+            output += f"   {rate[0]}{" " * (29 - len(rate[0]))}  {rate[1]:.6f}\n"
+        
+        return output
+
 
 class Route:
     path: str # file path to json data
-    source: str # name of planet
-    dest: str # name of planet
-    
-    def __init__(self) -> None:
-        pass
+    source: Planet # name of planet
+    dest: Planet # name of planet
+    waypoints: Dict[float, "Waypoint"] # from source to dest, invert as needed
+
+    def __init__(self, path: str, source: Planet, dest: Planet) -> None:
+        self.path = path
+        self.source = source
+        self.dest = dest
+        self.waypoints = dict()
+
+        # read in waypoint data
+        with open("./json/" + self.path, "r") as f:
+            data = json.load(f)
+            spawn_defs = data["space-connection"]["asteroid_spawn_definitions"]
+            for sd in spawn_defs:
+                name = sd["asteroid"]
+                for point in sd["spawn_points"]:
+                    distance = point["distance"]
+                    probability = point["probability"]
+                    # grab the right waypoint or create it
+                    try:
+                        waypoint = self.waypoints[distance]
+                    except KeyError:
+                        waypoint = Waypoint(distance)
+                        self.waypoints[distance] = waypoint
+                    # add a rate for this asteroid to the corresponding waypoint
+                    waypoint.add_rate(name, probability)
+
+        for w in self.waypoints.values():
+            print(w)
+
 
 def parse_route_file():
+    pass
     
 
 def main():
@@ -43,12 +89,16 @@ def main():
     route_files: List[str] = []
     
     for file in os.listdir("./json"):
+        print(file)
         if "_" in file:
             route_files.append(file)
         else:
             planet_files.append(file)
     
+    
     for route_file in route_files:
+        route = Route(route_file, Planet(), Planet())
+        return
         src_planet = route_file[0:route_file.find("_")]
         dest_planet = route_file[route_file.find("_") + 1:-5]
         
@@ -56,7 +106,7 @@ def main():
             planet_data = json.load(f)
         
     
-    gleba = [p for p in planet_files if "gleba" in p][0]
+    gleba = [p for p in planet_files if "nauvis" in p][0]
     
     with open("./json/" + gleba, 'r') as f:
         planet_data = json.load(f)
